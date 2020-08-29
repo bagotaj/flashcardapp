a2a36dc7faa334bd01e24919f91c5411822134b3
bagotaj <bagotaj@gmail.com>
2020-08-29 20:44:42 +0200

    Reorganize Files

--- bootstrap/js/databasemanipulation.js
+++
@@ -1,8 +0,0 @@
-function storeCards() {
-    localStorage.setItem(keyNames[usedDeck], JSON.stringify(storedFlashCards));
-}
-
-function getCards(index) {
-    storedFlashCards = localStorage.getItem(keyNames[index]);
-    storedFlashCards = storedFlashCards ? JSON.parse(storedFlashCards) : [];
-}
--- bootstrap/js/elementcreator.js
+++
@@ -1,8 +0,0 @@
-function createAnyElement(name, attributes) {
-    let element = document.createElement(name);
-
-    for (let k in attributes) {
-        element.setAttribute(k, attributes[k]);
-    }
-    return element;
-}
